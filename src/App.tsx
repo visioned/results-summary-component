f028046d4dcc3d5497aabaa53c7f154386c0f48e
visioned <ajkg11@gmail.com>
2023-02-20 03:06:36 -0800

    average score logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import data from "./data.json";
 import Result from "./components/Result";
 import Summary from "./components/Summary";
-
+interface IData {
+  category: string;
+  score: number;
+  icon: string;
+}
 function App() {
+  const scores = data.map((item: IData) => item.score);
+  const averageScore = scores.reduce((a, b) => a + b, 0) / scores.length;
 
   return (
     <main className="h-screen flex justify-center items-center">
       <div className="lg:flex lg:flex-row flex-col lg:h-[512px] lg:w-[736px] h-[825px] w-[375px] bg-[#fff] shadow-2xl rounded-[33px]">
-        <Result />
+        <Result averageScore={averageScore} />
         <Summary data={data} />
       </div>
     </main>
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,4 +1,8 @@
-const Result = () => {
+interface ResultProps {
+  averageScore: number;
+}
+
+const Result = (props: ResultProps) => {
   return (
     <div className="lg:p-10 p-8 lg:h-[512px] lg:w-[368px] h-[380px] bg-gradient-to-t from-[#2e2be9] to-[#7857ff] lg:rounded-[33px] rounded-b-[33px] flex-col flex items-center">
       <h2 className="lg:text-[22px] text-[18px] font-bold tracking-wide opacity-70">
@@ -8,7 +12,7 @@ const Result = () => {
         <div className="flex items-center justify-center">
           <div className="lg:mt-12 mt-8">
             <p className="flex flex-col items-center font-bold lg:text-[70px] text-[55px] leading-none text-white">
-              {}
+              {props.averageScore.toFixed(0)}
               <span className="text-base font-normal opacity-50">of 100</span>
             </p>
           </div>
